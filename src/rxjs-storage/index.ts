2de6fc1e099a1e8174e2a0d96872984e3766a3b6
orjandesmet <orjan.desmet@ordina.be>
2020-04-22 12:30:02 +0200

    Add schematic for required properties

--- src/rxjs-storage/index.ts
+++ src/rxjs-storage/index.ts
@@ -1,14 +1,17 @@
 import { strings } from '@angular-devkit/core';
 import { apply, mergeWith, Rule, template, url } from '@angular-devkit/schematics';
 
+interface Schema {
+  name: string;
+}
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function rxjsStorage(_options: any): Rule {
+export function rxjsStorage(_options: Schema): Rule {
   return copyFiles(_options);
 }
 
-function copyFiles(_options: any): Rule {
+function copyFiles(_options: Schema): Rule {
   const sourceTemplates = url('./files');
   const sourceParameterizedTemplates = apply(sourceTemplates, [
     template({
