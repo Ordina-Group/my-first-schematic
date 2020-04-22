f8e0005ccd84c9ad2296c81e45f9f211facbe105
orjandesmet <orjan.desmet@ordina.be>
2020-04-22 14:05:11 +0200

    Add Angular Workspace support

--- src/rxjs-storage/index.ts
+++ src/rxjs-storage/index.ts
@@ -1,9 +1,12 @@
 import { strings } from '@angular-devkit/core';
-import { apply, mergeWith, Rule, template, url } from '@angular-devkit/schematics';
+import { apply, mergeWith, Rule, template, url, Tree, SchematicsException, SchematicContext, move } from '@angular-devkit/schematics';
+import { parseName } from '@schematics/angular/utility/parse-name';
+import { buildDefaultPath } from '@schematics/angular/utility/project';
 
 interface Schema {
   name: string;
   props: string;
+  project?: string;
 }
 
 // You don't have to export the function as default. You can also have more than one rule factory
@@ -13,15 +16,31 @@ export function rxjsStorage(_options: Schema): Rule {
 }
 
 function copyFiles(_options: Schema): Rule {
-  const sourceTemplates = url('./files');
-  const sourceParameterizedTemplates = apply(sourceTemplates, [
-    template({
-      ..._options,
-      ...strings,
-      props: _options.props.split(',')
-      .filter((prop: string) => !!prop.trim())
-      .map((prop: string) => prop.split(':').map(value => value.trim()))
-    })
-  ]);
-  return mergeWith(sourceParameterizedTemplates);
+  return (tree: Tree, context: SchematicContext) => {
+    const { name, path } = getPath(tree, _options);
+    const sourceTemplates = url('./files');
+    const sourceParameterizedTemplates = apply(sourceTemplates, [
+      template({
+        ..._options,
+        ...strings,
+        props: _options.props.split(',')
+          .filter((prop: string) => !!prop.trim())
+          .map((prop: string) => prop.split(':').map(value => value.trim())),
+        name
+      }),
+      move(path)
+    ]);
+    return mergeWith(sourceParameterizedTemplates)(tree, context);
+  }
+}
+
+function getPath(tree: Tree, _options: Schema) {
+  const workspaceConfigBuffer = tree.read('angular.json');
+  if (!workspaceConfigBuffer) {
+    throw new SchematicsException("Not an Angular CLI workspace");
+  }
+  const workspaceConfig = JSON.parse(workspaceConfigBuffer.toString());
+  const project = workspaceConfig.projects[_options.project || workspaceConfig.defaultProject];
+  const defaultProjectPath = buildDefaultPath(project);
+  return parseName(defaultProjectPath, _options.name);
 }
